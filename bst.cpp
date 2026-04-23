9c0015bca876390ff033cfa26fa93e42b602622a
Yih-Chun Hu <yihchun@whatever_university_I_teach_at.edu>
2026-04-23 16:37:46 -0500

    iterator

--- bst.cpp
+++ bst.cpp
@@ -4,6 +4,9 @@ template <class T>
 class BSTree;
 
 template <class T>
+class BSTIterator;
+
+template <class T>
 class BSTNode {
     BSTNode(const T &d) : data(d) { left = right = nullptr; }
     ~BSTNode() { delete left; delete right; }
@@ -32,6 +35,60 @@ class BSTNode {
     T data;
     BSTNode<T> *left;
     BSTNode<T> *right;
+    friend class BSTree<T>;
+    friend class BSTIterator<T>;
+};
+
+template <class T>
+class BSTIterator : public std::iterator<std::forward_iterator_tag, T> {
+public:
+    BSTIterator() { }
+    BSTIterator(BSTNode<T> *root) { initalize(root); }
+    BSTIterator(const BSTIterator &from) { loc = from.loc; }
+    BSTIterator &operator=(const BSTIterator &from) {
+        loc = from.loc; return *this; }
+    bool operator==(const BSTIterator &with) const {
+        return loc == with.loc; }
+    bool operator!=(const BSTIterator &with) const {
+        return loc != with.loc; }
+    BSTIterator operator++() { step(); return *this; }
+    BSTIterator operator++(int) {
+        BSTIterator tmp(*this); step(); return tmp; }
+    T &operator*() const { return loc.top().first->data; }
+private:
+    void initalize(BSTNode<T> *root) {
+        while (root) {
+            loc.push(std::make_pair(root, 0));
+            root = root->left;
+        }
+        if (!loc.empty())
+            loc.top().second++;
+    }
+    void step() {
+        if (loc.empty()) return;
+        do {
+            switch (loc.top().second) {
+            case 0:
+                loc.top().second++;
+                break;
+            case 1:
+                loc.top().second++;
+                if (loc.top().first->right) {
+                    BSTNode<T> *walk = loc.top().first->right;
+                    while (walk) {
+                        loc.push(std::make_pair(walk, 0));
+                        walk = walk->left;
+                    }
+                    loc.top().second++;
+                }
+                break;
+            case 2:
+                loc.pop();
+                break;
+            }
+        } while (!loc.empty() && loc.top().second != 1);
+    }
+    std::stack<std::pair<BSTNode<T> *, int> > loc;
     friend class BSTree<T>;
 };
 
@@ -94,10 +151,14 @@ public:
         delete tmp;
     }
 
+    BSTIterator<T> begin() const { return BSTIterator<T>(root); }
+    BSTIterator<T> end() const { return BSTIterator<T>(); }
+
 };
 
 int main() {
     BSTree<int> t;
+    BSTree<int> t2;
     const BSTree<int> *x = &t;
     t.insert(5).insert(6).insert(3).insert(4).insert(7);
     std::cout << t.search(3) << std::endl;
@@ -105,8 +166,28 @@ int main() {
     std::cout << t.search(67) << std::endl;
     std::cout << t.height() << std::endl;
     x->print_inorder();
-    t.delete_node(5);
+    for (auto v: t2)
+        std::cout << v << " ";
+    std::cout << "\n";
+    
+    t.delete_node(4);
     t.print_inorder();
+    for (BSTIterator<int> it = t.begin(); it != t.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << "\n";
+    /* somehow, delete_node(4) crashes my system (MacOS). no idea why. 
+    t.delete_node(3);
+    t.print_inorder();
+    t.delete_node(4);
+    t.print_inorder();
+    t.delete_node(6);
+    t.print_inorder();
+    t.delete_node(7);
+    t.print_inorder();
+    */
+
+
     // not allowed: x->insert(0);
     return 0;
 }
